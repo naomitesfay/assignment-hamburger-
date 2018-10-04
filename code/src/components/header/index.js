511fd6e7904919b4e17f338da05dd00264f953a7
Naomi <naomi.tesfay@gmail.com>
2018-10-04 22:47:33 +0200

    assignment-hamburger

--- code/src/components/header/index.js
+++ code/src/components/header/index.js
@@ -8,11 +8,26 @@ class Header extends React.Component {
   render() {
     return (
       <header>
+
+        <label for="hamburger" class="hamburger-label" ></label>
+
         <Grid className="header-grid">
-          <a href="http://technigo.io">Technigo</a>
-          <a href="http://technigo.io">Boot Camp</a>
-          <a href="http://technigo.io">Stories</a>
-          <a href="http://technigo.io">About</a>
+        <label for="hamburger" className="hamburger-label">
+           <span className="hamburger-1"></span>
+           <span className="hamburger-2"></span>
+           <span className="hamburger-3"></span>
+         </label>
+
+         <input id="hamburger" type="checkbox" className="hamburger-checkbox" name="" value="" />
+
+         <nav class="menu">
+           <ul>
+             <li><a href="http://technigo.io">Technigo</a></li>
+             <li><a href="http://technigo.io">Boot Camp</a></li>
+             <li><a href="http://technigo.io">Stories</a></li>
+             <li><a href="http://technigo.io">About</a></li>
+           </ul>
+         </nav>
         </Grid>
       </header>
     )
